02ffd379a63ddee60b2edc31fc50e4f7a1bc7c54
shanmukh0504 <shanmukh0504@gmail.com>
2025-10-22 01:01:41 +0530

    Add AssetSupportNotice component to MainLayout
    - Integrated AssetSupportNotice into MainLayout for improved user notifications regarding asset support.
    - Adjusted layout to accommodate the new component with appropriate styling.

--- app/src/components/layout/MainLayout.tsx
+++ app/src/components/layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import AssetSupportNotice from '@/components/ui/AssetSupportNotice';
 import { cn } from '@/lib/utils';
 
 interface MainLayoutProps {
@@ -12,6 +13,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children, className }) => {
     return (
         <div className={cn('min-h-screen flex flex-col bg-[#F8F7F5]', className)}>
             <Navbar className="relative z-10 pb-8 md:pb-12" />
+            <AssetSupportNotice className="mb-6" />
             <main className="flex-1 relative z-0">
                 {children}
             </main>
